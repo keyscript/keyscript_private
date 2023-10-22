27520b0673135bdc4350aa2c6e9f17ab7679c7f7
ksang123 <etaizilberman@gmail.com>
2023-10-22 04:30:14 +0300

    html page to try out the compiled wasm file

--- src/compiler.rs
+++ src/compiler.rs
@@ -4,31 +4,27 @@ use wasm_encoder::{
 };
 use std::fs;
 
-
-
 pub fn wasm() {
     let mut module = Module::new();
 
-    //wasm magic shit:
-// Encode the type section.
     let mut types = TypeSection::new();
     let params = vec![ValType::I32, ValType::I32];
     let results = vec![ValType::I32];
     types.function(params, results);
     module.section(&types);
 
-// Encode the function section.
+    // Encode the function section.
     let mut functions = FunctionSection::new();
     let type_index = 0;
     functions.function(type_index);
     module.section(&functions);
 
-// Encode the export section.
+    // Encode the export section with the function named "add".
     let mut exports = ExportSection::new();
-    exports.export("f", ExportKind::Func, 0);
+    exports.export("add", ExportKind::Func, 0);
     module.section(&exports);
 
-// Encode the code section.
+    // Encode the code section.
     let mut codes = CodeSection::new();
     let locals = vec![];
     let mut f = Function::new(locals);
@@ -38,6 +34,7 @@ pub fn wasm() {
     f.instruction(&Instruction::End);
     codes.function(&f);
     module.section(&codes);
+
 
     //output:
     let wasm_bytes = module.finish();
